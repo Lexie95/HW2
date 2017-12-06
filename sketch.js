ff780ba5f6f2d6089cc11e4e587f9d6281b51e66
Lexie95 <32326197+Lexie95@users.noreply.github.com>
2017-12-06 01:11:06 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,28 @@
 function setup() {
  createCanvas(500, 500);
   angleMode(DEGREES);
+  
+  // How many times draw() runs every second?
   frameRate(40);
   background(50)
 }
 
 function draw() {
   translate(width/1.5,height/2);
-  noFill() 
+
+  // Every frame, it makes a rotation of 3 degrees
+  noFill()
+  
   rotate(frameCount)
+   // lerpColor blends two colors to find a third color somewhere between them.
+  // lerpColor(col1,col2,amount)
   stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/180));
-  if (frameCount == 180) {
-  noLoop();
+  line(0,0,200*sin(frameCount)-200*sin(frameCount),200*sin(frameCount));
+
+  
+    // Stops the draw function if a certain condition avaluates true
+    if (frameCount == 180) {
+    noLoop();
 
 }
 }
